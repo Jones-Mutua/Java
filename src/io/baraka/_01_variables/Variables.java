96e01ab8933c0fb400c9964770096b8385df6726
Baraka Mulumia <barakamulumia@gmail.com>
2021-06-17 13:13:05 +0300

    Create Variables.java
    Definition and working with variables in  java

---
+++ src/io/baraka/_01_variables/Variables.java
@@ -0,0 +1,24 @@
+package io.baraka._01_variables;
+
+public class Variables {
+    public static void main(String[] args) {
+            /*
+          we represent pieces of data in code
+          lets represent a student in code
+          (age, gpa, first-initial, last-initial, GPA, perfectAttendance)
+          a variable is a label/identifier
+         */
+        int studentAge  = 17;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'B';
+        char studentLastInitial ='M';
+        boolean hasPerfectAttendance = true;
+
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+    }
+}
