b65a6a2bccb1970513ec2aa859d5ca98ff0c6329
Baraka Mulumia <barakamulumia@gmail.com>
2021-06-17 18:22:11 +0300

    Update Comparison.java
    Complete Code on comparisons

--- src/io/baraka/_05_comparison/Comparison.java
+++ src/io/baraka/_05_comparison/Comparison.java
@@ -2,6 +2,23 @@ package io.baraka._05_comparison;
 
 public class Comparison {
     public static void main(String[] args) {
-        //TODO
+        int age = 17;
+        //greater
+        System.out.println(age > 10);
+
+        //less
+        System.out.println(age < 10);
+
+        //greater or equal
+        System.out.println(age >= 10);
+
+        //less or equal
+        System.out.println(age<= 10);
+
+        //not equal
+        System.out.println(age != 17);
+
+        //EQUAL
+        System.out.println(age == 17);
     }
 }
