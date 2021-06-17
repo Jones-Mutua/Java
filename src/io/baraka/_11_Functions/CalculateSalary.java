4f66d945f4e4cc1bdd6884e8cfb6e34c52a0229c
Baraka Mulumia <barakamulumia@gmail.com>
2021-06-17 15:33:56 +0300

    Create CalculateSalary.java
    Challenge:  Calculating Employee Gross Salary

---
+++ src/io/baraka/_11_Functions/CalculateSalary.java
@@ -0,0 +1,16 @@
+package io.baraka._11_Functions;
+
+        /*
+       create a function to calculate the employees salary
+       based on how many hours an employee works per week and how much an employee makes per hour
+       inputs [hours/week  pay/hour]
+       output: employees gross yearly salary
+       Bonus - add an input that accounts for the vacation days and the
+       employee does not get paid on vacation days assuming that a vacation day is equal to 8hrs of work
+        */
+public class CalculateSalary {
+
+    public static void main(String[] args) {
+
+    }
+}
