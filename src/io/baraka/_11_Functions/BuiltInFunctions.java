cf1e4eb526621b2efa83f0bb777254b507aa7379
Baraka Mulumia <barakamulumia@gmail.com>
2021-06-17 15:29:19 +0300

    Create BuiltInFunctions.java
    System Defined Functions in Java

---
+++ src/io/baraka/_11_Functions/BuiltInFunctions.java
@@ -0,0 +1,21 @@
+package io.baraka._11_Functions;
+
+public class BuiltInFunctions {
+    public static void main(String[] args) {
+        /*
+         There are many common functions that developers want to use
+         but do not want to define
+         Java has defined some functions for us
+         We can just call a built-in function because its already defined
+                 System.out.println();
+         */
+        String name = "Barak";
+        System.out.println(name.toLowerCase());
+
+        /*
+        Using Math for robust arithmetical operations
+
+         */
+        System.out.println(Math.pow(2, 5));;
+    }
+}
