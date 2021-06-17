889e32be858923ad4dbe451016c923d2cc19994b
Baraka Mulumia <barakamulumia@gmail.com>
2021-06-17 13:12:12 +0300

    Getting Started
    Setting-Up the development Environment
    Exploring how java code gets executed
    The Java Syntax

---
+++ src/com/baraka/java/Main.java
@@ -0,0 +1,9 @@
+package com.baraka.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //every java program must have a main class
+        System.out.println("Hello World Its Me!! Coding in Java");
+    }
+}
