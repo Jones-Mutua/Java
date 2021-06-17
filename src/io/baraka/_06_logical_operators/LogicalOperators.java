7d2c7e5908316bfeefeb19d9e2f40bba222904a1
Baraka Mulumia <barakamulumia@gmail.com>
2021-06-17 18:22:26 +0300

    Update LogicalOperators.java
    Complete code on Logical Operators

--- src/io/baraka/_06_logical_operators/LogicalOperators.java
+++ src/io/baraka/_06_logical_operators/LogicalOperators.java
@@ -2,6 +2,24 @@ package io.baraka._06_logical_operators;
 
 public class LogicalOperators {
     public static void main(String[] args) {
-        //TODO
+        int age = 17;
+        /*
+        and operator
+        evaluates to true if both conditions are true
+         */
+        System.out.println(age < 35 && age >18 );
+
+
+        /*
+        or operator
+        evaluates to true if any of the conditions are true
+         */
+        System.out.println(age < 35 || age >18  );
+
+       /*
+        and operator
+        flips or reverses the evaluation of a condition
+         */
+        System.out.println( !(age >18)  );
     }
 }
