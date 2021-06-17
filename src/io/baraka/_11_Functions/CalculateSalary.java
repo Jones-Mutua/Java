c4d84e42b17db1b3104aeacfa7b8e1419410e303
Baraka Mulumia <barakamulumia@gmail.com>
2021-06-17 15:36:54 +0300

    Update CalculateSalary.java
    Solution:  function to calculate the employees salary

--- src/io/baraka/_11_Functions/CalculateSalary.java
+++ src/io/baraka/_11_Functions/CalculateSalary.java
@@ -1,16 +1,26 @@
 package io.baraka._11_Functions;
 
-        /*
-       create a function to calculate the employees salary
-       based on how many hours an employee works per week and how much an employee makes per hour
-       inputs [hours/week  pay/hour]
-       output: employees gross yearly salary
-       Bonus - add an input that accounts for the vacation days and the
-       employee does not get paid on vacation days assuming that a vacation day is equal to 8hrs of work
-        */
+/*
+create a function to calculate the employees salary
+based on how many hours an employee works per week and how much an employee makes per hour
+inputs [hours/week  pay/hour]
+output: employees gross yearly salary
+Bonus - add an input that accounts for the vacation days and the
+employee does not get paid on vacation days assuming that a vacation day is equal to 8hrs of work
+*/
 public class CalculateSalary {
+    public static double calculateSalary(double hoursPerWeek, double amountPerHour, int vacationDays) {
+        if (hoursPerWeek < 1 || amountPerHour < 1) {
+            return -1;
+        }
+        double weeklyPayCheck = hoursPerWeek * amountPerHour;
+        double unpaidTime = vacationDays * amountPerHour * 8;
+
+        return (weeklyPayCheck * 52) - unpaidTime;
+    }
 
     public static void main(String[] args) {
-
+        double salary = calculateSalary(40, 15, 8);
+        System.out.println("$ " + salary);
     }
 }
