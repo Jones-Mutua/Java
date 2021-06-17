9adede6ab23dae239c75ac9beb3c6926cd2540fa
Baraka Mulumia <barakamulumia@gmail.com>
2021-06-17 21:25:54 +0300

    Create User.java
    Created a Superclass  (User) to explore Inheritance

---
+++ src/io/baraka/_12_classes/inheritance/User.java
@@ -0,0 +1,35 @@
+package io.baraka._12_classes.inheritance;
+
+public class User {
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String toString(){
+       return  "Hello There I am " + this.getUsername() + " Welcome to I-S-T";
+    }
+
+
+}
