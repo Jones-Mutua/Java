2e846718538c4b3e1380bb6b531691b410d8960c
Baraka Mulumia <barakamulumia@gmail.com>
2021-06-24 17:32:20 +0300

    Update CalculateSalary.java
    Solution

--- src/io/baraka/_11_Functions/CalculateSalary.java
+++ src/io/baraka/_11_Functions/CalculateSalary.java
@@ -6,11 +6,28 @@ package io.baraka._11_Functions;
        inputs [hours/week  pay/hour]
        output: employees gross yearly salary
        Bonus - add an input that accounts for the vacation days and the
-       employee does not get paid on vacation days assuming that a vacation day is equal to 8hrs of work
+       employee does not get paid on vacation days assuming
+       that a vacation day is equal to 8hrs of work
         */
 public class CalculateSalary {
 
-    public static void main(String[] args) {
+    public  static  double calculateGrossSalary(double hoursPerWeek,
+                                                double amountPerHour,
+                                                int vacationDays
+                                                ){
+        double weeklyPay = hoursPerWeek * amountPerHour;
+        double unpaidTime = vacationDays  * 8;
+        double unpaidAmount  = unpaidTime * amountPerHour;
 
+        return (weeklyPay * 52) - unpaidAmount;
+
+    }
+
+
+    public static void main(String[] args) {
+        double salary = calculateGrossSalary(40,
+                15,
+                10);
+        System.out.println(salary);
     }
 }
