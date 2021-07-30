edd571b3b21184a26d3848717b29984ff803e303
Baraka Mulumia <barakamulumia@gmail.com>
2021-07-30 21:21:34 +0300

    Create Main.java
    Introductory file

---
+++ src/io/baraka/_00_intro/Main.java
@@ -0,0 +1,25 @@
+package io.baraka._00_intro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        Before we begin, there are a few things to note
+        In java most of the code we are going to write will be within a class
+        Even though classes are covered as the last part of this coursework,
+        for now just know that most of the codes will need to be in a class
+
+        to show output, we are going to use  System.out.println function
+        to print the results of our code on the terminal
+
+        This code was developed using IntelliJ IDEA
+        The code is structured subtropical by use of packages,
+        something you do  not also need to worry about, you will be familiar with packages
+        later on.
+        if you are using any other software you will need to know how to run java code
+        Here in IntelliJ IDEA to run java code just press the play button
+
+         */
+        System.out.println("Hello World Its Me!! Coding in Java");
+    }
+}
