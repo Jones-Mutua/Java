9d707ef0abb158b03926cabe102fcb983109ee4c
Baraka Mulumia <barakamulumia@gmail.com>
2021-07-30 21:19:07 +0300

    Delete Main.java
    Delete com.baraka.java package and all it's contents

--- src/com/baraka/java/Main.java
+++
@@ -1,9 +0,0 @@
-package com.baraka.java;
-
-public class Main {
-
-    public static void main(String[] args) {
-        //every java program must have a main class
-        System.out.println("Hello World Its Me!! Coding in Java");
-    }
-}
